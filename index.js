ed92103de3b9e0a6eeab513b6c70bc9fa5056a99
f0x <f0x@cthu.lu>
2021-04-08 17:13:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const puppeteer = require("puppeteer");
+const Promise = require("bluebird");
+const express = require("express");
+const mqtt = require("mqtt");
+
+const port = 3199;
+const app = express();
+const mqttClient = mqtt.connect("mqtt://[2a0e:5700:4:11::53]");
+
+app.get("/mqtt", (req, res) => {
+	console.log("PARTICIPANTS", req.query.participants);
+	mqttClient.publish('revspace/j', req.query.participants.toString(), {retain: true});
+	res.send("ack");
+});
+
+app.get("/console", (req, res) => {
+	console.log(req.query.log);
+	console.log("Log from browser:", ...Object.values(JSON.parse(req.query.log)));
+	res.send("ack");
+});
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+	console.log("started");
+});
+
+let page;
+let browser;
+
+Promise.try(() => {
+	return puppeteer.launch({
+		headless: true,
+		args: ["--no-sandbox"]
+	});
+}).then((res) => {
+	browser = res;
+	const context = browser.defaultBrowserContext();
+	context.clearPermissionOverrides();
+	return browser.newPage();
+}).then((res) => {
+	page = res;
+	return page.goto(`http://localhost:${port}`);
+}).then(() => {
+	page.on('dialog', (dialog) => {
+		console.log(dialog);
+	});
+});
